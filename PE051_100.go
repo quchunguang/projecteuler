da667cca292368e3ff80e0c5bbeb8a94265bcb0e
Kevin Qu <quchunguang@gmail.com>
2014-10-31 18:17:48 +0800

    PE71 solved

--- PE051_100.go
+++ PE051_100.go
@@ -1357,7 +1357,31 @@ func PE70(N int64) (ret int64) {
 
 // Problem 71 - Ordered fractions
 //
-func PE71() (ret int) {
+// Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+// If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+//
+//   1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+//
+// It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+// By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
+func PE71(D int64) (ret int64) {
+	var max, nd float64
+	s37 := big.NewRat(3, 7)
+	r := float64(3) / float64(7)
+	for d := D; d > 1; d-- {
+		n := int(float64(d) * r)
+		s := big.NewRat(int64(n), int64(d))
+		if s.Cmp(s37) == 0 {
+			n--
+			s = big.NewRat(int64(n), int64(d))
+		}
+		nd = float64(n) / float64(d)
+		if nd > max {
+			max = nd
+			ret = s.Num().Int64()
+			// fmt.Println(s)
+		}
+	}
 	return
 }
 
