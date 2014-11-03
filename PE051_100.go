d801bd76c9c91c8bfcd4f83d2c52c598be95019e
Kevin Qu <quchunguang@gmail.com>
2014-11-03 21:43:41 +0800

    PE73 solved

--- PE051_100.go
+++ PE051_100.go
@@ -1412,7 +1412,30 @@ func PE72(D int64) (ret int64) {
 
 // Problem 73 - Counting fractions in a range
 //
-func PE73() (ret int) {
+// Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+// If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+//
+// 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+//
+// It can be seen that there are 3 fractions between 1/3 and 1/2.
+// How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d ≤ 12,000?
+// slow: about 5 minutes
+func PE73(D int64) (ret int64) {
+	v13 := big.NewRat(1, 3)
+	v12 := big.NewRat(1, 2)
+	var d, n int64
+	for d = 2; d <= D; d++ {
+		if d%1000 == 0 {
+			fmt.Print(EL, "d = ", d)
+		}
+		for n = 1; n < d; n++ {
+			v := big.NewRat(n, d)
+			if v13.Cmp(v) == -1 && v12.Cmp(v) == 1 && v.Num().Int64() == n {
+				ret++
+			}
+		}
+	}
+	fmt.Print(EL)
 	return
 }
 
