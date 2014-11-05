163b2c6baaec8a128bb2c77ba44d737826632cb6
Kevin Qu <quchunguang@gmail.com>
2014-11-05 11:10:45 +0800

    little

--- PE051_100.go
+++ PE051_100.go
@@ -1545,7 +1545,15 @@ func PE75(L int) (ret int) {
 
 // Problem 76 - Counting summations
 //
-func PE76() (ret int) {
+// It is possible to write five as a sum in exactly six different ways:
+//   4 + 1
+//   3 + 2
+//   3 + 1 + 1
+//   2 + 2 + 1
+//   2 + 1 + 1 + 1
+//   1 + 1 + 1 + 1 + 1
+// How many different ways can one hundred be written as a sum of at least two positive integers?
+func PE76(N int) (ret int) {
 	return
 }
 
